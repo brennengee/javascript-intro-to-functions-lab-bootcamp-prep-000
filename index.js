0c4da3dc546c3ae614e13739a274353b5ea107d3
Brennen Gee <brennengee@me.com>
2017-12-06 23:19:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function whisper(string) {
 
 function logShout(string) {
   var shout = logShout(string)
-  var shshout = 
-  console.log(.toUpperCase(string))
+  var shshout = shout.toUpperCase
+  console.log(shshout)
 }
