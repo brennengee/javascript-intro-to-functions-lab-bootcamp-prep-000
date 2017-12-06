3d0db80b23891f6ef566aba0b65677d66736cfde
Brennen Gee <brennengee@me.com>
2017-12-06 23:44:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   var casecase = ${string}
   if (casecase.toUpperCase() === casecase) {
   return string
-  } else ()
-  
-
+  } else (casecase.toLowerCase() === casecase) {
+    return "I can't hear you!"
+  }
 }
